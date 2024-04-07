41156fe57575cb9130deb933f37bb9930da7622e
Thragull <146970246+Thragull@users.noreply.github.com>
2024-04-07 13:54:07 +0000

    Added all database structure for the client section Added some tables from employees section Added Commands to fill most of the tables created.

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,13 +1,14 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Models, Configurations, Fleet, Prices
+from .models import (db, User, Models, Configurations, Fleet, Prices, Projects, Assignations, Budgets, Roles, Countries,
+                    Nationalities, States, Employees)
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    app.config['FLASK_ADMIN_SWATCH'] = 'united'
+    admin = Admin(app, name='Jetstream DataBase', template_mode='bootstrap3')
 
     
     # Add your models here, for example this is how we add a the User model to the admin
@@ -15,6 +16,14 @@ def setup_admin(app):
     admin.add_view(ModelView(Configurations, db.session))
     admin.add_view(ModelView(Fleet, db.session))
     admin.add_view(ModelView(Prices, db.session))
+    admin.add_view(ModelView(Projects, db.session))
+    admin.add_view(ModelView(Assignations, db.session))
+    admin.add_view(ModelView(Budgets, db.session))
+    admin.add_view(ModelView(Roles, db.session))
+    admin.add_view(ModelView(Countries, db.session))
+    admin.add_view(ModelView(Nationalities, db.session))
+    admin.add_view(ModelView(States, db.session))
+    admin.add_view(ModelView(Employees, db.session))
     admin.add_view(ModelView(User, db.session))
 
     # You can duplicate that line to add mew models
--- src/api/models.py
+++ src/api/models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
 #Database Design:
-#Fleet section:
+#Database structure for Fleet:
 
 class Models(db.Model):
     __tablename__ = 'models'
@@ -80,6 +81,170 @@ class Prices(db.Model):
             "configuration": self.configuration_id,
             "crew": self.crew,
             "price": self.price
+        }
+    
+#Database structure for Projects:
+    
+class Projects(db.Model):
+    __tablename__ = 'projects'
+    id =  db.Column(db.Integer, primary_key=True)
+    project = db.Column(db.String(50), unique=True, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return "Project {}".format(self.project)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "project": self.project,
+            "start": self.start_date,
+            "end": self.end_date
+        }
+
+class Assignations(db.Model):
+    __tablename__ = 'assignations'
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer,db.ForeignKey('projects.id'), nullable=False)
+    project = db.relationship(Projects)
+    aircraft_id = db.Column(db.Integer, db.ForeignKey('fleet.id'), nullable=False)
+    aircraft = db.relationship(Fleet)
+
+    def __repr__(self):
+        return 'Aircraft {} is assigned to project {}'.format(self.aircraft.registration, self.project.project)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "project_id": self.project_id,
+            "aircraft_id": self.aircraft_id
+        }
+
+
+class Budgets(db.Model):
+    __tablename__ = 'budgets'
+    id = db.Column(db.Integer, primary_key=True)
+    client_name = db.Column(db.String(50), nullable=False)
+    client_surname = db.Column(db.String(50), nullable=False)
+    client_business = db.Column(db.String(50), nullable=False)
+    client_email = db.Column(db.String(150), nullable=False)
+    client_phone = db.Column(db.BigInteger, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return "We can negotiate with {}".format(self.client_business)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.client_name,
+            "surname": self.client_surname,
+            "business": self.client_business,
+            "email": self.client_email,
+            "phone": self.client_phone,
+            "start": self.start_date,
+            "end": self.end_date,
+            "price": self.total_price
+        }
+    
+#Database structure for Employees registration
+    
+class Roles(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(15), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"{self.role}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "role": self.role
+        }
+
+class Countries(db.Model):
+    __tablename__ = 'countries'
+    id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"{self.country}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "country": self.country
+        }
+
+class Nationalities(db.Model):
+    __tablename__ = 'nationalities'
+    id = db.Column(db.Integer, primary_key=True)
+    nationality = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"{self.nationality}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nationality": self.nationality
+        }
+
+class States(db.Model):
+    __tablename__ = 'states'
+    id = db.Column(db.Integer, primary_key=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable=False)
+    country = db.relationship(Countries)
+    state = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return "{} from {}".format(self.state, self.country.country)
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "country": self.country_id,
+            "state": self.state
+        }
+
+class Employees(db.Model):
+    __tablename__ = 'employees'
+    id = db.Column(db.Integer, primary_key=True)
+    crew_id = db.Column(db.String(3), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    role = db.relationship(Roles)
+    department = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    nationality_id = db.Column(db.Integer, db.ForeignKey('nationalities.id'))
+    nationality = db.relationship(Nationalities)
+    address = db.Column(db.String(250))
+    address2 = db.Column(db.String(250))
+    address3 = db.Column(db.String(250))
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
+    country = db.relationship(Countries)
+    state_id = db.Column(db.Integer, db.ForeignKey('states.id'))
+    state = db.relationship(States)
+    city = db.Column(db.String(50))
+    zipcode = db.Column(db.Integer)
+    birthday = db.Column(db.DateTime)
+    entry_date = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return "{} {} with code ({})".format(self.name, self.surname, self.crew_id)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "crew_id": self.crew_id,
+            
         }
 
 class User(db.Model):
