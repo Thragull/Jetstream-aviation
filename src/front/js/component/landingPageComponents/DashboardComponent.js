559ce076345713d01ceaffbe5d6c2ce81704f569
jollaib <56800825+jollaib@users.noreply.github.com>
2024-05-07 09:31:59 +0000

    Animacion en construccion

--- src/front/js/component/landingPageComponents/DashboardComponent.js
+++ src/front/js/component/landingPageComponents/DashboardComponent.js
@@ -1,13 +1,31 @@
 import React, { useContext } from "react";
 import { Context } from "../../store/appContext";
+import Lottie from "react-lottie";
+import animationData from "../../../img/construction_animation.json";
 
 
 export const DashboardComponent = () => {
 	const { store, actions } = useContext(Context);
 
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData, 
+        renderSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+    }
+}
+
 	return (
         <div>
             <h1>Dashboard</h1>
+            <div className="mt-5">
+                <Lottie
+                    options={defaultOptions}
+                    height={400}
+                    width={400}
+                />
+            </div>
         </div>
 	);
 };
